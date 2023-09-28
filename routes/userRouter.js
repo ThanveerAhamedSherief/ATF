918e58f4a28d3a99f179623236eed6baf14b06df
Thanveer Sherief - vn55jeb <thanveer.sherief@walmart.com>
2023-09-29 00:12:16 +0530

    testing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { registerUser, login } = require("../controllers/userController");
 const userRouter = express.Router();
 
-userRouter.post("/signin", registerUser).post("/login", login);
+userRouter.post("/signin", registerUser).post("/login", login).post('/test',(req,res) =>{
+    res.status(200).send("its working")
+})
+
 
 exports.userRouter = userRouter;
